a00e4332e14e01f9d0ba1b9d8f01a8e02dabcc97
Otto Jung <otto.jung@vauplace.com>
2023-06-17 02:04:02 -0700

    Implement a workaround of #lang-less source files
    Dynamically create a file that has the #lang header
    and imports the actual desired file.

--- r7rs-lib/private/import.rkt
+++ r7rs-lib/private/import.rkt
@@ -2,6 +2,8 @@
 
 (require (for-syntax racket/base
                      racket/format
+                     syntax/modresolve
+                     racket/file
                      racket/string
                      syntax/parse))
 
@@ -57,7 +59,49 @@
              #:with require-spec
              #'name.module-path)))
 
+(begin-for-syntax
+  (define (make-import-file target-path rkt-path)
+    (define-values (dirname/p basename/p whatever)
+      (split-path target-path))
+    (define basename (path->string basename/p))
+    (call-with-output-file
+     rkt-path #:exists 'truncate
+     (lambda (p)
+       (write
+        `(module _ r7rs (include ,basename))
+        p)))
+    rkt-path)
+
+  (define (require-spec->module-id first)
+    (if (list? first)
+        (case (car first)
+          ((only-in) (list-ref first 1))
+          ((except-in) (list-ref first 1))
+          ((rename-in) (list-ref first 1))
+          ((prefix-in) (list-ref first 2))
+          (else (car first)))
+        first))
+
+  (define (handle-import resolved)
+    (define (try-path target-path)
+      (and (file-exists? target-path)
+           (make-import-file target-path resolved)))
+
+    (or (file-exists? resolved)
+        (try-path (path-replace-extension resolved ".sld"))
+        (try-path (path-replace-extension resolved ".scm"))
+        (try-path (path-replace-extension resolved ".ss"))
+        (try-path (path-replace-extension resolved ".sls"))))
+
+  )
+
 (define-syntax (import stx)
   (syntax-parse stx
     [(_ spec:import-spec ...)
-     #'(require spec.require-spec ...)]))
+     (let* ((fullspec (syntax->datum #'(spec.require-spec ...)))
+            (mods (map require-spec->module-id fullspec))
+            (resolveds (map resolve-module-path mods))
+            )
+       (for-each handle-import resolveds)
+       #'(require spec.require-spec ...))
+     ]))
